eeef18c5fada36a32b3e562f54a51930fad178de
steflap <stephanelapointe@gmail.com>
2026-05-07 14:53:04 -0400

    chore(deploy): #16 M5r one-shot clear cache prod (auto-delete)

---
+++ public/clear-s84-m5r.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->call('optimize:clear');
+echo "CACHE CLEARED " . date('Y-m-d H:i:s') . "\n";
+@unlink(__FILE__);
