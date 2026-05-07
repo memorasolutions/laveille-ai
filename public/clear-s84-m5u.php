7fc4157370b4b0cb9b7efd2d03c82f8574cff925
steflap <stephanelapointe@gmail.com>
2026-05-07 15:21:07 -0400

    chore(deploy): #16 M5u clear view+cache (auto-delete)

---
+++ public/clear-s84-m5u.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
+$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
+$kernel->call('view:clear');
+$kernel->call('optimize:clear');
+echo "CACHE+VIEW CLEARED M5u " . date('Y-m-d H:i:s') . "\n";
+@unlink(__FILE__);
